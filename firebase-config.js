7b3e478716616ae8722a8215b5806b4a70c3066f
Ditzzy124 <dyyykayyy@gmail.com>
2026-09-07 12:07:55 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// GANTI dengan Firebase Web App config milikmu.
+// Jangan menaruh password admin/service account di website.
+export const firebaseConfig = {
+  apiKey: "ISI_API_KEY",
+  authDomain: "ISI_PROJECT.firebaseapp.com",
+  projectId: "ISI_PROJECT_ID",
+  storageBucket: "ISI_STORAGE_BUCKET",
+  messagingSenderId: "ISI_MESSAGING_SENDER_ID",
+  appId: "ISI_APP_ID"
+};
